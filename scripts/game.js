39766af79c14ccc328443f18d6e3afed0208257d
MadeleineWalder <madeleinezoewalder@gmail.com>
2023-03-01 09:29:55 +0000

    Add most tests and code

--- scripts/game.js
+++ scripts/game.js
@@ -2,18 +2,67 @@ let game = {
     score: 0,
     currentGame: [],
     playerMoves: [],
-    choices: ["button1", "button2", "button3", "button4"],
-};
+    turnNumber: 0,
+    choices: ["button1", "button2", "button3", "button4"]
+}
+
+// Note: why don't these functions have ; on the end?
 
 function newGame() {
-    game.score = 0;
     game.currentGame = [];
     game.playerMoves = [];
+    game.score = 0;
+    for (let circle of document.getElementsByClassName("circle")) {
+        if (circle.getAttribute("data-listener") !== "true") {
+            circle.addEventListener("click", (e) => {
+                let move = e.target.getAttribute("id");
+                lightsOn(move);
+                game.playerMoves.push(move);
+                playerTurn();
+            });
+            circle.setAttribute("data-listener", "true");
+        }
+    }
     showScore();
+    addTurn();
+}
+
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    showTurns();
 }
 
 function showScore() {
     document.getElementById("score").innerText = game.score;
 }
 
-module.exports = { game, newGame, showScore };
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add("light");
+    setTimeout(() => {
+        document.getElementById(circ).classList.remove("light");
+    }, 400);
+}
+
+// showTurns: "I'm calling the lightsOn function,  
+// inside a JavaScript set interval.  Which just makes sure that we have  
+// a little pause between the lights being  shown and the next step in the sequence.
+// Then we've added an if statement inside  here, as well that if our turnNumber  
+// is equal or over the length of our  current game array. Then obviously,  
+// the sequence is finished so  we can clear our interval."
+// "As you can see, it's setting this  interval turning the lightsOn,  
+// incrementing the game turnNumber,  and then turning them off again."
+
+function showTurns() {
+    game.turnNumber = 0;
+    let turns = setInterval(() => {
+        lightsOn(game.currentGame[game.turnNumber]);
+        game.turnNumber++;
+        if (game.turnNumber >= game.currentGame.length) {
+            clearInterval(turns);
+        }
+    }, 800);
+}
+
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };
